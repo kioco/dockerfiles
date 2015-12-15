0c7d7828419586a8cbc8cedc118c6413107a39fb
Piotr Bochenski <piotr.bochenski@nokia.com>
2015-12-15 14:18:55 +0100

    bump Cassandra version to 3.0.1

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build -t="bochen/datastax-base" datastax-base
 docker build -t="bochen/opscenter:5.2.2" opscenter
-docker build -t="bochen/cassandra:3.0.0" cassandra
+docker build -t="bochen/cassandra:3.0.1" cassandra
 docker build -t="bochen/elasticsearch:2.1.0" elasticsearch
 docker build -t="bochen/kibana:4.3.0" kibana
 docker build -t="bochen/zookeeper:3.4.7" zookeeper
